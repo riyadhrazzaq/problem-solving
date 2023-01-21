d77a88371c720f5452dd2d9f375c9d71b00be02e
riyadhrazzaq <riyadh.razzaq@gmail.com>
2023-01-22 00:39:36 +0600

    added cf 1371A

---
+++ codeforces/CF_1371A.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class CF_1371A {
+
+    public void run() throws IOException {
+        // WRITE YOUR CODE HERE
+        int t = nextInt();
+        while (t-- > 0) {
+           int n = nextInt();
+           int s = (int) Math.ceil(n / (double) 2);
+           out.println(s);
+        }
+    }
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    PrintWriter out = new PrintWriter(System.out);
+    StringTokenizer in = new StringTokenizer("");
+
+    boolean hasNext() throws IOException {
+        if (in.hasMoreTokens()) return true;
+        String s;
+        while ((s = br.readLine()) != null) {
+            in = new StringTokenizer(s);
+            if (in.hasMoreTokens()) return true;
+        }
+        return false;
+    }
+
+    String nextToken() throws IOException {
+        while (!in.hasMoreTokens()) {
+            in = new StringTokenizer(br.readLine());
+        }
+        return in.nextToken();
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(nextToken());
+    }
+
+    double nextDouble() throws IOException {
+        return Double.parseDouble(nextToken());
+    }
+
+    long nextLong() throws IOException {
+        return Long.parseLong(nextToken());
+    }
+
+    public <T extends Object> void toString(Iterable<T> iterable) {
+        System.out.print("[");
+        for (T t : iterable) {
+            System.out.print(t + ", ");
+        }
+        System.out.println("]");
+    }
+
+    public void test() {
+
+        try {
+            br = new BufferedReader(new FileReader(this.getClass().getSimpleName() + ".test"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        CF_1371A m = new CF_1371A();
+        m.test();
+        m.run();
+        m.out.close();
+    }
+}
